e94b803fac5550fd0217736322b686e5fb412e68
jamesspalding <jameshspalding01@gmail.com>
2024-01-13 22:07:51 -0600

    setting up shinylive

---
+++ App/app.R
@@ -0,0 +1,31 @@
+source("functions.R")) #already sources setup.R, so no need to load again.
+
+ui = fluidPage(
+  titlePanel("LENSS Graphs"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      dateInput("input_date", "Enter a date between 7/19/23-11/8/23:"),
+      checkboxInput("checkbox", "Show maximum SQM reading")
+    ),
+    
+    mainPanel(
+      plotOutput("result_plot")
+    )
+  )
+)
+
+server = function(input, output, session) {
+  output$result_plot = renderPlot({
+    if (input$checkbox) {
+      result_plot = SQMGraph(as.Date(input$input_date))
+      print(result_plot)
+    }else{
+      result_plot = dateGraph(as.Date(input$input_date))
+      print(result_plot)
+    }
+  })
+}
+
+shinyApp(ui, server)
+
