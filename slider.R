808bb58ffe476258a408d9a30a365d9ef336a2ec
jamesspalding <jameshspalding01@gmail.com>
2024-06-01 12:58:43 -0500

    update to work with new function

--- slider.R
+++ slider.R
@@ -1,37 +1,43 @@
-source("functions.R") #already sources setup.R, so no need to load again.
+source("setup.R")
+source("buildGraph.R")
 
 
-ui <- fluidPage(
-    titlePanel("LENSS Graphs"),
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("input_date",
-                        "Select Date:",
-                        min = as.Date("2023-07-20"),
-                        max = as.Date("2023-11-08"),
-                        value = as.Date("2023-07-20"),
-                        timeFormat = "%Y-%m-%d"),
-            
-            checkboxInput("checkbox", "Show maximum SQM reading")
-        ),
-        
-        mainPanel(
-             plotOutput("result_plot")
-        )
-        
+ui = fluidPage(
+       titlePanel("LENSS Graphs"),
+       sidebarLayout(
+          sidebarPanel(
+              sliderInput("input_date",
+                          "Select Date:",
+                          min = as.Date("2023-07-20"), #First available date
+                          max = as.Date("2023-11-08"), #Last available date
+                          value = as.Date("2023-07-20"),
+                          timeFormat = "%Y-%m-%d"),
+              
+              checkboxInput("midCheck", "Show midnight line"),
+              checkboxInput("sqmCheck", "Show maximum SQM reading"),
+              checkboxInput("bortleCheck", "Show Bortle scale")
+              
+          ),
+          
+          mainPanel(
+               plotOutput("result_plot")
+          )
+          
+       )
     )
-)
 
 
 server = function(input, output, session) {
   output$result_plot = renderPlot({
-    if (input$checkbox) {
-      result_plot = SQMGraph(as.Date(input$input_date))
-      print(result_plot)
-    }else{
-      result_plot = dateGraph(as.Date(input$input_date))
-      print(result_plot)
-    }
+    
+    sqmVal = input$sqmCheck
+    midVal = input$midCheck
+    bortleVal = input$bortleCheck
+    
+    print(buildGraph(as.Date(input$input_date),
+                     midVal,
+                     sqmVal,
+                     bortleVal))
   })
 }
 
