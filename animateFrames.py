e25d77dbffd6da0da6024739078d7c0af5d0d03a
jamesspalding <jameshspalding01@gmail.com>
2024-06-12 17:40:42 -0500

    gif creation functions

---
+++ animateFrames.py
@@ -0,0 +1,20 @@
+import os
+import shutil
+from PIL import Image
+
+def animateFrames(directory, output_file="animated_plot.gif", duration=200):
+    images = []
+    for filename in sorted(os.listdir(directory)):
+        if filename.endswith('.png'):
+            img_path = os.path.join(directory, filename)
+            images.append(Image.open(img_path))
+    
+    images[0].save(
+        output_file,
+        save_all=True,
+        append_images=images[1:],
+        duration=duration,
+        loop=0)
+
+    #remove tempframes
+    shutil.rmtree(directory)
