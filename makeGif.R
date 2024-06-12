21eb40dc6ecb4c785ce7ee51f0ee0ad49f30c500
jamesspalding <jameshspalding01@gmail.com>
2024-06-12 17:48:36 -0500

    updated code to be more concise

--- makeGif.R
+++ makeGif.R
@@ -1,11 +1,23 @@
-# Uses generateFrames and animateFrames to create gif
+# creates a gif within specified range
 
 makeGif <- function(start_date, end_date,
-                    MID = F, SQM = F, BORTLE = F, SIZE = c(3201,1800)){ #makegraph params
+                           MID = F, SQM = F, BORTLE = F, SIZE = c(3201,1800), #makegraph params
+                           duration = 200){ #gif duration
   
+  date_diff = as.Date(end_date) - as.Date(start_date)
   dirName = paste0(getwd(), "/tempframes")
   
-  generateFrames(start_date, end_date, MID, SQM, BORTLE, SIZE)
+  for(time_elapsed in 0:date_diff){
+    framedate = as.Date(start_date) + time_elapsed
+    buildGraph(framedate, midLine = MID, sqm = SQM, bortle = BORTLE)
+
+    ggsave(filename = paste0(dirName, "/frame", time_elapsed, ".png"),
+           plot = last_plot(),
+           width = SIZE[1],
+           height = SIZE[2],
+           units = "px")
+  }
   
-  animateFrames(dirName)
+  animateFrames(dirName, duration = duration)
+
 }
